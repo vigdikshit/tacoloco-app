cb633e87170da2734b05596b53e81d25851dc23e
Dikshit vig <dikshit.vig@gmail.com>
2021-02-24 15:09:35 -0500

    Initial commit- Spring boot project created

---
+++ app/app/src/main/java/com/tacoLoco/app/TacoLocoApplication.java
@@ -0,0 +1,13 @@
+package com.tacoLoco.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoLocoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoLocoApplication.class, args);
+	}
+
+}
---
+++ app/app/src/test/java/com/tacoLoco/app/TacoLocoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tacoLoco.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoLocoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
